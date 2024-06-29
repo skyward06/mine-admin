93b6fa195ec913e18eff3acda5f086694b2dbb0b
Skyward <skywardwander06@gmail.com>
2024-06-29 00:19:26 -0400

    Add navbar icons in public

--- src/layouts/config-nav-dashboard.tsx
+++ src/layouts/config-nav-dashboard.tsx
@@ -11,8 +11,31 @@ const icon = (name: string) => (
 );
 
 const ICONS = {
+  job: icon('ic_job'),
+  blog: icon('ic_blog'),
+  chat: icon('ic_chat'),
+  mail: icon('ic_mail'),
   user: icon('ic_user'),
+  file: icon('ic_file'),
+  lock: icon('ic_lock'),
+  tour: icon('ic_tour'),
+  order: icon('ic_order'),
+  label: icon('ic_label'),
+  blank: icon('ic_blank'),
   school: icon('ic_school'),
+  kanban: icon('ic_kanban'),
+  folder: icon('ic_folder'),
+  banking: icon('ic_banking'),
+  booking: icon('ic_booking'),
+  invoice: icon('ic_invoice'),
+  product: icon('ic_product'),
+  calendar: icon('ic_calendar'),
+  disabled: icon('ic_disabled'),
+  external: icon('ic_external'),
+  menuItem: icon('ic_menu_item'),
+  ecommerce: icon('ic_ecommerce'),
+  analytics: icon('ic_analytics'),
+  dashboard: icon('ic_dashboard'),
 };
 
 // ----------------------------------------------------------------------
@@ -23,6 +46,9 @@ export const navData = [
    */
   {
     subheader: 'Texitcoin Administration',
-    items: [{ title: 'User', path: paths.dashboard.user.root, icon: ICONS.user }],
+    items: [
+      { title: 'Dashboard', path: paths.dashboard.history.root, icon: ICONS.analytics },
+      { title: 'User', path: paths.dashboard.user.root, icon: ICONS.user },
+    ],
   },
 ];
