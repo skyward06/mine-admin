77f839497e38f5e5864f09fce64bd10052d2b122
Skyward <skywardwander06@gmail.com>
2024-07-08 12:41:07 -0400

    Customize chart on dashboard section

---
+++ src/sections/dashboard/Summary.tsx
@@ -0,0 +1,68 @@
+import { useQuery as useGraphQuery } from '@apollo/client';
+
+import Grid from '@mui/material/Unstable_Grid2';
+import { alpha, useTheme } from '@mui/material/styles';
+
+import ChartWidget from 'src/components/ChartWidget';
+
+import { FETCH_STATISTICS_QUERY } from './query';
+
+export default function Summary() {
+  const theme = useTheme();
+
+  const { data: statisticsData } = useGraphQuery(FETCH_STATISTICS_QUERY, {
+    variables: {
+      page: '1,30',
+      sort: 'issuedAt',
+    },
+  });
+
+  const statistics = statisticsData?.statistics ?? { statistics: [], total: 0 };
+
+  return (
+    <Grid container spacing={3}>
+      <Grid xs={12} md={6}>
+        <ChartWidget
+          title="Daily"
+          chart={{
+            categories: statistics
+              ?.statistics!.map((item) => new Date(item!.issuedAt).toISOString().split('T')[0])
+              .reverse(),
+            series: [
+              {
+                name: 'New Blocks',
+                data: statistics!.statistics!.map((item) => item!.newBlocks).reverse(),
+              },
+            ],
+            options: {
+              plotOptions: {
+                bar: {
+                  columnWidth: '80%',
+                },
+              },
+            },
+            colors: [alpha(theme.palette.primary.dark, 0.8)],
+          }}
+          type="bar"
+        />
+      </Grid>
+      <Grid xs={12} md={6}>
+        <ChartWidget
+          title="TXC Shared"
+          chart={{
+            categories: statistics
+              ?.statistics!.map((item) => new Date(item!.issuedAt).toISOString().split('T')[0])
+              .reverse(),
+            series: [
+              {
+                name: 'Daily Reward',
+                data: statistics!.statistics!.map((item) => item!.txcShared).reverse(),
+              },
+            ],
+            colors: [theme.palette.warning.main],
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+}
--- src/sections/dashboard/index.tsx
+++ src/sections/dashboard/index.tsx
@@ -1,26 +1,13 @@
-import { useQuery as useGraphQuery } from '@apollo/client';
-
-import Grid from '@mui/material/Unstable_Grid2';
 import Container from '@mui/material/Container';
 
 import { paths } from 'src/routes/paths';
 
-import ChartWidget from 'src/components/ChartWidget';
 import { Breadcrumbs } from 'src/components/Breadcrumbs';
 
+import Summary from './Summary';
 import StatisticsTable from './StatisticsTable';
-import { FETCH_STATISTICS_QUERY } from './query';
 
 export default function Dashboard() {
-  const { data: statisticsData } = useGraphQuery(FETCH_STATISTICS_QUERY, {
-    variables: {
-      page: '1,30',
-      sort: 'issuedAt',
-    },
-  });
-
-  const statistics = statisticsData?.statistics ?? { statistics: [], total: 0 };
-
   return (
     <Container maxWidth="xl">
       <Breadcrumbs
@@ -31,52 +18,7 @@ export default function Dashboard() {
         }}
       />
 
-      <Grid container spacing={3}>
-        <Grid xs={12} md={6}>
-          <ChartWidget
-            title="Daily"
-            chart={{
-              categories: statistics
-                ?.statistics!.map((item) => new Date(item!.issuedAt).toISOString().split('T')[0])
-                .reverse(),
-              series: [
-                {
-                  name: 'New Blocks',
-                  data: statistics!.statistics!.map((item) => item!.newBlocks).reverse(),
-                },
-                {
-                  name: 'New Hash Power',
-                  data: statistics!.statistics!.map((item) => item!.totalHashPower).reverse(),
-                },
-              ],
-              options: {
-                plotOptions: {
-                  bar: {
-                    columnWidth: '16%',
-                  },
-                },
-              },
-            }}
-            type="bar"
-          />
-        </Grid>
-        <Grid xs={12} md={6}>
-          <ChartWidget
-            title="TXC Shared"
-            chart={{
-              categories: statistics
-                ?.statistics!.map((item) => new Date(item!.issuedAt).toISOString().split('T')[0])
-                .reverse(),
-              series: [
-                {
-                  name: 'Daily Reward',
-                  data: statistics!.statistics!.map((item) => item!.newBlocks * 254).reverse(),
-                },
-              ],
-            }}
-          />
-        </Grid>
-      </Grid>
+      <Summary />
       <StatisticsTable />
     </Container>
   );
