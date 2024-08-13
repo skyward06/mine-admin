2c5222b1ce4be63481a78f10a45d2660b76c3696
Skyward <skywardwander06@gmail.com>
2024-08-13 11:15:53 -0400

    Customize export button in tabletoolbar

--- src/sections/Statistics/TableToolBar.tsx
+++ src/sections/Statistics/TableToolBar.tsx
@@ -1,17 +1,56 @@
+import axios from 'axios';
+
 import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { GridToolbarExport, GridToolbarContainer } from '@mui/x-data-grid';
+import { GridToolbarContainer } from '@mui/x-data-grid';
+
+import { fDate, fTime } from 'src/utils/format-time';
+
+import { CONFIG } from 'src/config';
+
+import { Iconify } from 'src/components/Iconify';
 
 interface AccountTableToolBarProps {
+  target: string;
+  token: string;
   setFilterButtonEl: React.Dispatch<React.SetStateAction<HTMLButtonElement | null>>;
 }
 
-export function TableToolBar({ setFilterButtonEl }: AccountTableToolBarProps) {
+export function TableToolBar({ target, token, setFilterButtonEl }: AccountTableToolBarProps) {
+  const handleExport = async () => {
+    const { data } = await axios.get(`${CONFIG.SITE_URL}/api/export-${target}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      responseType: 'arraybuffer',
+    });
+
+    const blob = new Blob([data], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `rewards_${fDate(new Date(), 'YYYYMMDD')}${fTime(new Date(), 'hhmmss')}.xlsx`;
+
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <GridToolbarContainer>
       <Typography variant="subtitle1">Reward</Typography>
       <Stack spacing={1} flexGrow={1} direction="row" alignItems="center" justifyContent="flex-end">
-        <GridToolbarExport />
+        {token && (
+          <Button variant="text" startIcon={<Iconify icon="uil:export" />} onClick={handleExport}>
+            Export
+          </Button>
+        )}
       </Stack>
     </GridToolbarContainer>
   );
