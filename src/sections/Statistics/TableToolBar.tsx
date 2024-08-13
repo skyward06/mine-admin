6dff23e29b91a67fd0bf9c769912ccc9e32ec064
Skyward <skywardwander06@gmail.com>
2024-08-13 11:34:17 -0400

    Add export button component

--- src/sections/Statistics/TableToolBar.tsx
+++ src/sections/Statistics/TableToolBar.tsx
@@ -1,15 +1,8 @@
-import axios from 'axios';
-
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { GridToolbarContainer } from '@mui/x-data-grid';
 
-import { fDate, fTime } from 'src/utils/format-time';
-
-import { CONFIG } from 'src/config';
-
-import { Iconify } from 'src/components/Iconify';
+import ExportButton from 'src/components/ExportButton';
 
 interface AccountTableToolBarProps {
   target: string;
@@ -18,39 +11,11 @@ interface AccountTableToolBarProps {
 }
 
 export function TableToolBar({ target, token, setFilterButtonEl }: AccountTableToolBarProps) {
-  const handleExport = async () => {
-    const { data } = await axios.get(`${CONFIG.SITE_URL}/api/export-${target}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      responseType: 'arraybuffer',
-    });
-
-    const blob = new Blob([data], {
-      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    });
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = `rewards_${fDate(new Date(), 'YYYYMMDD')}${fTime(new Date(), 'hhmmss')}.xlsx`;
-
-    document.body.appendChild(a);
-    a.click();
-
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-  };
-
   return (
     <GridToolbarContainer>
       <Typography variant="subtitle1">Reward</Typography>
       <Stack spacing={1} flexGrow={1} direction="row" alignItems="center" justifyContent="flex-end">
-        {token && (
-          <Button variant="text" startIcon={<Iconify icon="uil:export" />} onClick={handleExport}>
-            Export
-          </Button>
-        )}
+        {token && <ExportButton target={target} token={token} />}
       </Stack>
     </GridToolbarContainer>
   );
