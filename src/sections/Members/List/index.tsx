e41c47d104ce5df305d0f554f667656de4dd6a66
Skyward <skywardwander06@gmail.com>
2024-08-15 02:16:35 -0400

    Change id name to username

--- src/sections/Members/List/index.tsx
+++ src/sections/Members/List/index.tsx
@@ -55,7 +55,7 @@ const STATUS_OPTIONS: { value: MemberRole; label: string; color: LabelColor }[] 
 ];
 
 const TABLE_HEAD = [
-  { id: 'name', label: 'Name', sortable: true },
+  { id: 'username', label: 'Name', sortable: true },
   { id: 'sponsor.username', label: 'Sponsor Name', width: 150, sortable: true },
   { id: 'mobile', label: 'Mobile', width: 150, sortable: true },
   { id: 'primaryAddress', label: 'Address', sortable: true },
