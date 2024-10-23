52cc52d20bf5f9d8aa48bfbeaec526119e7c16d6
Skyward <skywardwander06@gmail.com>
2024-10-23 11:54:43 -0400

    Enable commission, approve

--- src/layouts/config-nav-dashboard.tsx
+++ src/layouts/config-nav-dashboard.tsx
@@ -61,7 +61,7 @@ export const navData = [
       { title: 'Product', path: paths.dashboard.products.root, icon: ICONS.package },
       { title: 'Sponsor', path: paths.dashboard.sponsor.root, icon: ICONS.sponsor },
       { title: 'Placement', path: paths.dashboard.placement.root, icon: ICONS.diagram },
-      // { title: 'Commission', path: paths.dashboard.commission.root, icon: ICONS.bonus },
+      { title: 'Commission', path: paths.dashboard.commission.root, icon: ICONS.bonus },
       { title: 'Reward', path: paths.dashboard.reward.root, icon: ICONS.reward },
       { title: 'Report', path: paths.dashboard.report.root, icon: ICONS.report },
       { title: 'Admin', path: paths.dashboard.user.root, icon: ICONS.admin },
