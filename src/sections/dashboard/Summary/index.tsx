deca39b34e07125e0d4f54dba3d39dae86e65b9f
Skyward <skywardwander06@gmail.com>
2024-12-06 00:58:01 -0500

    Cleanup chart in dashboard

--- src/sections/dashboard/Summary/index.tsx
+++ src/sections/dashboard/Summary/index.tsx
@@ -1,12 +1,13 @@
 import Grid from '@mui/material/Unstable_Grid2';
 
-import Revenue from './Revenue';
-import HashRate from './HashRate';
+import Revenue from 'src/sections/Statistics/Chart/Revenue';
+import HashRate from 'src/sections/Statistics/Chart/HashRate';
+import TotalMiner from 'src/sections/Statistics/Chart/TotalMiner';
+import Commission from 'src/sections/Statistics/Chart/Commission';
+import MemberCount from 'src/sections/Statistics/Chart/MemberCount';
+import MemberReward from 'src/sections/Statistics/Chart/MemberReward';
+
 import TXCShared from './TXCShared';
-import TotalMiner from './TotalMiner';
-import Commission from './Commission';
-import MemberCount from './MemberCount';
-import MemberReward from './MemberReward';
 
 export default function Summary() {
   return (
