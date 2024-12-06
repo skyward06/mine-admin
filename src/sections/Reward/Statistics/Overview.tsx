4d55046bba78eec642d8f7b38513e4204ba9b3c4
Skyward <skywardwander06@gmail.com>
2024-12-06 17:50:52 -0500

    Update statistics component

--- src/sections/Reward/Statistics/Overview.tsx
+++ src/sections/Reward/Statistics/Overview.tsx
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Unstable_Grid2';
 
 import { fNumber } from 'src/utils/formatNumber';
 import { formatDate } from 'src/utils/format-time';
@@ -13,53 +14,65 @@ interface Props {
 
 export default function Overview({ data }: Props) {
   return (
-    <Card sx={{ py: 3, textAlign: 'center', typography: 'h4' }}>
-      <Stack
-        direction="row"
-        divider={<Divider orientation="vertical" flexItem sx={{ borderStyle: 'dashed' }} />}
-      >
-        <Stack width={1}>
-          {fNumber(data?.newBlocks ?? 0)}
-          <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
-            New Blocks
-          </Box>
-        </Stack>
+    <Grid container spacing={3}>
+      <Grid lg={6} xs={12}>
+        <Card sx={{ py: 3, textAlign: 'center', typography: 'h4' }}>
+          <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" flexItem sx={{ borderStyle: 'dashed' }} />}
+          >
+            <Stack width={1}>
+              {fNumber(data?.newBlocks ?? 0)}
+              <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
+                New Blocks
+              </Box>
+            </Stack>
 
-        <Stack width={1}>
-          {fNumber(data?.totalBlocks ?? 0)}
-          <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
-            Total Blocks
-          </Box>
-        </Stack>
+            <Stack width={1}>
+              {fNumber(data?.totalBlocks ?? 0)}
+              <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
+                Total Blocks
+              </Box>
+            </Stack>
 
-        <Stack width={1}>
-          {fNumber(data?.totalMembers ?? 0)}
-          <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
-            Total Members
-          </Box>
-        </Stack>
+            <Stack width={1}>
+              {fNumber(data?.totalMembers ?? 0)}
+              <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
+                Total Members
+              </Box>
+            </Stack>
+          </Stack>
+        </Card>
+      </Grid>
+      <Grid lg={6} xs={12}>
+        <Card sx={{ py: 3, textAlign: 'center', typography: 'h4' }}>
+          <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" flexItem sx={{ borderStyle: 'dashed' }} />}
+          >
+            <Stack width={1}>
+              {fNumber((data?.txcShared ?? 0) / 10 ** 8)}
+              <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
+                Rewarded TXC
+              </Box>
+            </Stack>
 
-        <Stack width={1}>
-          {fNumber((data?.txcShared ?? 0) / 10 ** 8)}
-          <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
-            Rewarded TXC
-          </Box>
-        </Stack>
+            <Stack width={1}>
+              {fNumber(data?.totalHashPower ?? 0)}
+              <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
+                Hash Power
+              </Box>
+            </Stack>
 
-        <Stack width={1}>
-          {fNumber(data?.totalHashPower ?? 0)}
-          <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
-            Hash Power
-          </Box>
-        </Stack>
-
-        <Stack width={1}>
-          {formatDate(data?.issuedAt ?? new Date())}
-          <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
-            Issued At
-          </Box>
-        </Stack>
-      </Stack>
-    </Card>
+            <Stack width={1}>
+              {formatDate(data?.issuedAt ?? new Date())}
+              <Box component="span" sx={{ color: 'text.secondary', typography: 'body2' }}>
+                Issued At
+              </Box>
+            </Stack>
+          </Stack>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
