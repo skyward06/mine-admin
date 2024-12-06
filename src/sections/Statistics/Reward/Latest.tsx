5e1d6a5e4b1d7cafcc4edadb333ca219dd2dae8d
Skyward <skywardwander06@gmail.com>
2024-12-06 05:20:06 -0500

    Update latest reward compoonent

--- src/sections/Statistics/Reward/Latest.tsx
+++ src/sections/Statistics/Reward/Latest.tsx
@@ -6,8 +6,8 @@ import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
-import Divider from '@mui/material/Divider';
 import Skeleton from '@mui/material/Skeleton';
+import Grid from '@mui/material/Unstable_Grid2';
 import CardHeader from '@mui/material/CardHeader';
 import Typography from '@mui/material/Typography';
 
@@ -54,7 +54,8 @@ export default function Latest() {
           ) : (
             <Stack sx={{ mt: 2 }}>
               {latest.map((item) => (
-                <Stack
+                <Grid
+                  container
                   sx={{
                     gap: 1,
                     minWidth: 120,
@@ -62,37 +63,43 @@ export default function Latest() {
                     py: 1,
                     alignItems: 'center',
                   }}
-                  direction="row"
-                  divider={<Divider sx={{ borderStyle: 'dashed', p: 0 }} />}
                 >
-                  <Stack width={0.3} direction="row" columnGap={1} sx={{ alignItems: 'center' }}>
-                    <Tooltip title="New Blocks" arrow placement="top">
-                      <Iconify icon="clarity:block-line" width={18} />
-                    </Tooltip>
-                    <Typography variant="body1">{item.newBlocks}</Typography>
-                  </Stack>
-                  <Stack width={0.3} direction="row" columnGap={1} sx={{ alignItems: 'center' }}>
-                    <Tooltip title="Total Miners" arrow placement="top">
-                      <Iconify icon="stash:user-group" width={18} />
-                    </Tooltip>
-                    <Typography variant="body1">{item.totalMembers}</Typography>
-                  </Stack>
-                  <Stack width={0.5} direction="row" columnGap={1} sx={{ alignItems: 'center' }}>
-                    <Tooltip title="TXC Shared" arrow placement="top">
-                      <Avatar
-                        src={`${CONFIG.SITE_PATH}/assets/icons/brands/txc.png`}
-                        sx={{ width: 15, height: 15 }}
-                      />
-                    </Tooltip>
-                    <Typography variant="body1">{item.txcShared}</Typography>
-                  </Stack>
-                  <Stack width={0.3} direction="row" columnGap={1} sx={{ alignItems: 'center' }}>
-                    <Tooltip title="Issued At" arrow placement="top">
-                      <Iconify icon="stash:data-date-duotone" width={18} />
-                    </Tooltip>
-                    <Typography variant="body1">{formatDate(item.issuedAt)}</Typography>
-                  </Stack>
-                </Stack>
+                  <Grid md={2} xs={12}>
+                    <Stack direction="row" columnGap={1} sx={{ alignItems: 'center' }}>
+                      <Tooltip title="New Blocks" arrow placement="top">
+                        <Iconify icon="clarity:block-line" width={18} />
+                      </Tooltip>
+                      <Typography variant="body1">{item.newBlocks}</Typography>
+                    </Stack>
+                  </Grid>
+                  <Grid md={2} xs={12}>
+                    <Stack direction="row" columnGap={1} sx={{ alignItems: 'center' }}>
+                      <Tooltip title="Total Miners" arrow placement="top">
+                        <Iconify icon="stash:user-group" width={18} />
+                      </Tooltip>
+                      <Typography variant="body1">{item.totalMembers}</Typography>
+                    </Stack>
+                  </Grid>
+                  <Grid md={4} xs={12}>
+                    <Stack direction="row" columnGap={1} sx={{ alignItems: 'center' }}>
+                      <Tooltip title="TXC Shared" arrow placement="top">
+                        <Avatar
+                          src={`${CONFIG.SITE_PATH}/assets/icons/brands/txc.png`}
+                          sx={{ width: 15, height: 15 }}
+                        />
+                      </Tooltip>
+                      <Typography variant="body1">{item.txcShared}</Typography>
+                    </Stack>
+                  </Grid>
+                  <Grid md={2} xs={12}>
+                    <Stack direction="row" columnGap={1} sx={{ alignItems: 'center' }}>
+                      <Tooltip title="Issued At" arrow placement="top">
+                        <Iconify icon="stash:data-date-duotone" width={18} />
+                      </Tooltip>
+                      <Typography variant="body1">{formatDate(item.issuedAt)}</Typography>
+                    </Stack>
+                  </Grid>
+                </Grid>
               ))}
             </Stack>
           )}
