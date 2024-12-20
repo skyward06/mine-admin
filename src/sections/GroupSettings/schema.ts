bd180c06462d10156e6ea2716670b0a9d5cb1fc3
Skyward <skywardwander06@gmail.com>
2024-12-20 03:16:32 -0500

    Update group settings schema

--- src/sections/GroupSettings/schema.ts
+++ src/sections/GroupSettings/schema.ts
@@ -5,8 +5,8 @@ export type SchemaType = zod.infer<typeof Schema>;
 export const Schema = zod.object({
   name: zod.string({ required_error: 'Name is required' }),
   limitDate: zod.string({ required_error: 'Limit Date is required' }),
-  sponsorBonusPackageId: zod.string({ required_error: 'Sponsor Package is required' }),
-  rollSponsorBonusPackageId: zod.string({ required_error: 'Roll Sponsor Package is required' }),
+  sponsorBonusPackageId: zod.string().nullable(),
+  rollSponsorBonusPackageId: zod.string().nullable(),
   groupSettingCommissionBonuses: zod.array(
     zod.object({
       commission: zod.number(),
