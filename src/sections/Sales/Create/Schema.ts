5e18bf288558ae58911def85aa01b36768287264
Skyward <skywardwander06@gmail.com>
2024-12-23 14:52:21 -0500

    Add schema as const

---
+++ src/sections/Sales/Create/Schema.ts
@@ -0,0 +1,19 @@
+import { z as zod } from 'zod';
+
+export type SchemaType = zod.infer<typeof Schema>;
+
+export const Schema = zod.object({
+  orderedAt: zod.string({ required_error: 'Ordered At is required' }),
+  paymentMethod: zod.string({ required_error: 'Payment Method is required' }),
+  status: zod.number({ required_error: 'Status is required' }).default(1),
+  note: zod.string().optional().nullable(),
+  reflinks: zod
+    .array(
+      zod.object({
+        linkType: zod.string(),
+        link: zod.string(),
+      })
+    )
+    .optional()
+    .nullable(),
+});
---
+++ src/sections/Sales/Edit/Schema.ts
@@ -0,0 +1,20 @@
+import { z as zod } from 'zod';
+
+export type SchemaType = zod.infer<typeof Schema>;
+
+export const Schema = zod.object({
+  orderedAt: zod.string({ required_error: 'Ordered At is required' }),
+  paymentMethod: zod.string({ required_error: 'Payment Method is required' }),
+  status: zod.boolean({ required_error: 'Status is required' }).default(true),
+  packageId: zod.string({ required_error: 'Package is required' }),
+  note: zod.string().optional().nullable(),
+  reflinks: zod
+    .array(
+      zod.object({
+        linkType: zod.string(),
+        link: zod.string(),
+      })
+    )
+    .optional()
+    .nullable(),
+});
