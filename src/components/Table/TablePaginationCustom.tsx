be8abe08c884ee4145ae10ce635b6144b8680b46
Skyward <skywardwander06@gmail.com>
2025-01-21 15:27:29 -0500

    Update table pagination

--- src/components/Table/TablePaginationCustom.tsx
+++ src/components/Table/TablePaginationCustom.tsx
@@ -18,7 +18,7 @@ export function TablePaginationCustom({
   sx,
   dense,
   onChangeDense,
-  rowsPerPageOptions = [5, 10, 25],
+  rowsPerPageOptions = [5, 10, 25, 50, 100],
   ...other
 }: TablePaginationCustomProps) {
   return (
