75e140b555bfdfaa1f5c24836f5203cdd2e901ec
Skyward <skywardwander06@gmail.com>
2025-01-22 14:43:14 -0500

    Add export button

--- src/sections/Reports/List/index.tsx
+++ src/sections/Reports/List/index.tsx
@@ -1,7 +1,10 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 import { useTabs } from 'src/hooks/use-tabs';
 
@@ -26,9 +29,40 @@ const TABS = [
 // ----------------------------------------------------------------------
 export default function ReportView() {
   const tabs = useTabs('revenue');
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleTabChange = (event: any, newValue: any) => {
     tabs.onChange(event, newValue);
+  };
+
+  const handleExport = async () => {
+    setLoading(true);
+
+    const token = localStorage.getItem(CONFIG.storageTokenKey);
+
+    const { data } = await axios.get(`${CONFIG.SITE_URL}/api/export-member-in-out-revenues`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      responseType: 'arraybuffer',
+    });
+
+    const blob = new Blob([data], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `revenue.xlsx`;
+
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+
+    setLoading(false);
   };
 
   return (
@@ -43,6 +77,18 @@ export default function ReportView() {
           sx={{
             mb: { xs: 2, md: 3 },
           }}
+          action={
+            <LoadingButton
+              variant="contained"
+              startIcon={<Iconify icon="uil:export" />}
+              loading={loading}
+              color="primary"
+              onClick={handleExport}
+              sx={{ mb: 1 }}
+            >
+              Export
+            </LoadingButton>
+          }
         />
 
         <Tabs value={tabs.value} onChange={handleTabChange} sx={{ mb: { xs: 2, md: 3 } }}>
