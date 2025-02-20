8d2d6b8f8d811176b666f2b741466a04aa2e227f
Skyward <skywardwander06@gmail.com>
2025-02-20 08:37:02 -0500

    Update placemnet tree in miner

--- src/sections/Members/Edit/Placement/index.tsx
+++ src/sections/Members/Edit/Placement/index.tsx
@@ -1,4 +1,4 @@
-import type { Member } from 'src/__generated__/graphql';
+import type { Member, PlacementMember } from 'src/__generated__/graphql';
 
 import { isEmpty } from 'lodash';
 import { useMemo, useState, useEffect, useCallback } from 'react';
@@ -6,6 +6,7 @@ import { ReactFlow, type Node, type Edge, type FitViewOptions } from '@xyflow/re
 
 import Stack from '@mui/material/Stack';
 
+import { PlacementPosition } from 'src/__generated__/graphql';
 import {
   PLACEMENTTREE_NODE_WIDTH,
   PLACEMENTTREE_NODE_HEIGHT,
@@ -17,10 +18,10 @@ import { EmptyContent } from 'src/components/EmptyContent';
 import ComponentBlock from 'src/components/Component-Block';
 import { LoadingScreen } from 'src/components/loading-screen';
 
-import { StandardNode } from './node';
 import CustomEdge from './customEdge';
+import { StandardNode } from './node';
 import NodeContext from './nodeContext';
-import { useFetchMembers } from '../../useApollo';
+import { useFetchPlacementOMembers } from '../../useApollo';
 
 const fitViewOptions: FitViewOptions = {
   padding: 0.2,
@@ -34,127 +35,101 @@ interface Props {
   currentMember: Member;
 }
 
+const nodeTypes = {
+  treeNode: StandardNode,
+};
+
 export default function PlacementListView({ currentMember }: Props) {
   const [visibleMap, setVisibleMap] = useState<Record<string, number>>({});
-  const { loading, members, fetchMembers } = useFetchMembers();
+  const { loading, members, fetchPlacementMembers } = useFetchPlacementOMembers();
+
+  const exSetVisibleMap = useCallback((newVisibleMap: Record<string, number>) => {
+    setVisibleMap(newVisibleMap);
+    localStorage.setItem('placementVisibleMap', JSON.stringify(newVisibleMap));
+  }, []);
 
   useEffect(() => {
-    fetchMembers({ variables: { filter: { status: true }, sort: '-placementPosition' } });
+    fetchPlacementMembers();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentMember]);
 
-  const buildPlacementTree = (data: any[]) => {
+  type PlacementTreeNode = PlacementMember & { children: PlacementTreeNode[] };
+
+  function buildPlacementTree(miners: any[]) {
     const memberMap: Record<string, any> = {};
-    const memberProcess: any[] = [];
     let result: any = {};
 
-    data.forEach((member) => {
+    miners.forEach((member) => {
       memberMap[member.id] = { ...member, children: [] };
 
-      if (member.placementParentId) {
-        memberProcess.push(member);
-      }
-
-      if (member.id === currentMember?.id) {
+      if (member.id === currentMember.id) {
         result = memberMap[member.id];
       }
     });
 
-    memberProcess.forEach((member) => {
-      if (memberMap[member.placementParentId] && member.id !== member.placementParentId) {
+    miners.forEach((member) => {
+      if (member.id !== member.placementParentId) {
         memberMap[member.placementParentId!].children.push(memberMap[member.id]);
       }
     });
 
-    return result;
-  };
+    return { result, memberMap };
+  }
 
-  function buildTree(node: any, baseX: number, depth: number, tree: any[], vMap: any = null) {
-    const children = node.children.sort(
-      (child1: any, child2: any) =>
-        child1.placementPosition === 'LEFT' || child2.placementPosition === 'RIGHT'
-    );
+  function buildTree(root: PlacementTreeNode, vMap: Record<string, number> | null = null) {
+    const resultNodes: Node[] = [];
+    const depthHeights: number[] = [];
 
-    if (children.length === 0) {
-      const element = {
+    function func(node: PlacementTreeNode, startX: number, depth: number, tree: Node[]) {
+      const leftChild = node.children.filter(
+        (child) => child.placementPosition === PlacementPosition.Left
+      )[0];
+      const rightChild = node.children.filter(
+        (child) => child.placementPosition === PlacementPosition.Right
+      )[0];
+      const baseX = Math.max(depthHeights[depth] ?? 0, startX);
+      let positionX = baseX;
+      const positionY = depth * (PLACEMENTTREE_NODE_HEIGHT + PLACEMENTTREE_NODE_Y_SPACE);
+
+      if (!vMap || vMap[node.id] === 2) {
+        if (leftChild && rightChild) {
+          const childStartX = baseX - (PLACEMENTTREE_NODE_WIDTH + PLACEMENTTREE_NODE_X_SPACE) / 2;
+          const { endX: leftEndX } = func(leftChild, childStartX, depth + 1, tree);
+          const { endX: rightEndX } = func(rightChild, leftEndX, depth + 1, tree);
+          positionX =
+            (leftEndX + rightEndX - 2 * (PLACEMENTTREE_NODE_X_SPACE + PLACEMENTTREE_NODE_WIDTH)) /
+            2;
+        } else if (leftChild) {
+          const childStartX = baseX - (PLACEMENTTREE_NODE_WIDTH + PLACEMENTTREE_NODE_X_SPACE) / 2;
+          const { endX: leftEndX } = func(leftChild, childStartX, depth + 1, tree);
+          positionX = leftEndX - (PLACEMENTTREE_NODE_X_SPACE + PLACEMENTTREE_NODE_WIDTH) / 2;
+        } else if (rightChild) {
+          const childStartX = baseX + (PLACEMENTTREE_NODE_WIDTH + PLACEMENTTREE_NODE_X_SPACE) / 2;
+          const { endX: rightEndX } = func(rightChild, childStartX, depth + 1, tree);
+          positionX = rightEndX - ((PLACEMENTTREE_NODE_X_SPACE + PLACEMENTTREE_NODE_WIDTH) * 3) / 2;
+        }
+      }
+      const element: Node = {
         id: node.id,
-        data: { label: <StandardNode {...node} /> },
-        position: { x: baseX, y: depth * (PLACEMENTTREE_NODE_HEIGHT + PLACEMENTTREE_NODE_Y_SPACE) },
-        draggable: true,
+        position: { x: positionX, y: positionY },
+        data: node,
         style: {
-          padding: 0,
-          border: 'none',
-          borderRadius: '12px',
           width: PLACEMENTTREE_NODE_WIDTH,
           height: PLACEMENTTREE_NODE_HEIGHT,
         },
-        maxX: baseX + PLACEMENTTREE_NODE_WIDTH,
+        type: 'treeNode',
       };
 
       tree.push(element);
 
-      return element;
+      const endX = positionX + PLACEMENTTREE_NODE_WIDTH + PLACEMENTTREE_NODE_X_SPACE;
+      depthHeights[depth] = endX;
+
+      return { element, endX };
     }
 
-    let maxX = baseX;
-
-    if (!vMap || vMap[node.id] === 2) {
-      children
-        .filter((child: any) => child.placementPosition === 'LEFT')
-        .forEach((child: any) => {
-          const { maxX: tempX } = buildTree(
-            child,
-            maxX + PLACEMENTTREE_NODE_X_SPACE,
-            depth + 1,
-            tree,
-            vMap
-          );
-          maxX = tempX;
-        });
-    }
-
-    const res = {
-      id: node.id,
-      data: { label: <StandardNode {...node} /> },
-      position: {
-        x: Math.max(baseX, maxX - (PLACEMENTTREE_NODE_WIDTH - PLACEMENTTREE_NODE_X_SPACE) / 2),
-        y: depth * (PLACEMENTTREE_NODE_HEIGHT + PLACEMENTTREE_NODE_Y_SPACE),
-      },
-      draggable: true,
-      style: {
-        padding: 0,
-        border: 'none',
-        borderRadius: '12px',
-        width: PLACEMENTTREE_NODE_WIDTH,
-        height: PLACEMENTTREE_NODE_HEIGHT,
-      },
-    };
-
-    maxX = res.position.x + (PLACEMENTTREE_NODE_WIDTH - PLACEMENTTREE_NODE_X_SPACE) / 2;
-
-    if (!vMap || vMap[node.id] === 2) {
-      children
-        .filter((child: any) => child.placementPosition === 'RIGHT')
-        .forEach((child: any) => {
-          const { maxX: tempX } = buildTree(
-            child,
-            maxX + PLACEMENTTREE_NODE_X_SPACE,
-            depth + 1,
-            tree,
-            vMap
-          );
-          maxX = tempX;
-        });
-    }
-
-    const element = {
-      ...res,
-      maxX: Math.max(maxX, res.position.x + PLACEMENTTREE_NODE_WIDTH),
-    };
-
-    tree.push(element);
-
-    return element;
+    func(root, 0, 0, resultNodes);
+    return resultNodes;
   }
 
   function getMemberIdsWithDepth(node: any, depth: number, targetDepth: number) {
@@ -171,65 +146,58 @@ export default function PlacementListView({ currentMember }: Props) {
   }
 
   const nodes: Node[] = useMemo(() => {
-    if (!members || members.length === 0) return [];
-    const placementTree = buildPlacementTree(members.filter((member) => member?.placementParentId));
+    if (members.length === 0) return [];
+    const { result: placementTree } = buildPlacementTree(members);
+    if (!placementTree) return [];
 
-    const resultTree: any[] = [];
-
-    if (!isEmpty(placementTree)) {
-      buildTree(placementTree, 0, 0, resultTree, visibleMap);
-    }
-
-    return resultTree;
+    return buildTree(placementTree, visibleMap);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [members, visibleMap]);
 
   const edges: Edge[] = useMemo(
     () =>
       members
-        .filter((member) => member?.placementParentId)
+        .filter((member) => member.placementParentId !== member.id)
         .map((member) => ({
-          id: `${member?.placementParentId}:${member?.id}`,
-          source: member?.placementParentId ?? '',
-          target: member?.id ?? '',
-          type: 'customEdge',
+          id: `${member.placementParentId}:${member.id}`,
+          source: member.placementParentId,
+          target: member.id,
+          type: 'default',
         })),
     [members]
   );
 
   const expandTree = useCallback(
-    (id: string) => {
+    async (id: string) => {
       const newVisibleMap: Record<string, number> = { ...visibleMap };
 
       members
-        .filter((mb) => mb?.placementParentId === id)
-        .forEach((mb) => {
-          if (!newVisibleMap[mb?.id ?? '']) {
-            newVisibleMap[mb?.id ?? ''] =
-              members.findIndex((mber) => mber?.placementParentId === mb?.id) === -1 ? 3 : 1;
+        .filter((member) => member.placementParentId === id)
+        .forEach((member) => {
+          if (!newVisibleMap[member.id]) {
+            newVisibleMap[member.id] =
+              members.findIndex((mbr) => mbr.placementParentId === member.id) === -1 ? 3 : 1;
           }
         });
 
-      newVisibleMap[id] = 2;
+      newVisibleMap[id] =
+        members.findIndex((member) => member.placementParentId === id) === -1 ? 3 : 2;
 
-      setVisibleMap(newVisibleMap);
-
-      localStorage.setItem('placementVisibleMap', JSON.stringify(visibleMap));
+      exSetVisibleMap(newVisibleMap);
     },
-    [members, visibleMap]
+    [members, visibleMap, exSetVisibleMap]
   );
 
   const collapseTree = useCallback(
-    (id: string) => {
+    async (id: string) => {
       const newVisibleMap: Record<string, number> = { ...visibleMap };
 
-      newVisibleMap[id] = 1;
+      newVisibleMap[id] =
+        members.findIndex((member) => member.placementParentId === id) === -1 ? 3 : 1;
 
-      setVisibleMap(newVisibleMap);
-
-      localStorage.setItem('placementVisibleMap', JSON.stringify(newVisibleMap));
+      exSetVisibleMap(newVisibleMap);
     },
-    [visibleMap]
+    [members, visibleMap, exSetVisibleMap]
   );
 
   const contextValue = useMemo(
@@ -289,6 +257,7 @@ export default function PlacementListView({ currentMember }: Props) {
                     fitView
                     fitViewOptions={fitViewOptions}
                     edgeTypes={edgeTypes}
+                    nodeTypes={nodeTypes}
                   />
                 </NodeContext.Provider>
               </Stack>
--- src/sections/Members/Edit/Placement/node.tsx
+++ src/sections/Members/Edit/Placement/node.tsx
@@ -1,4 +1,7 @@
+import type { PlacementMember } from 'src/__generated__/graphql';
+
 import { useContext } from 'react';
+import { Handle, Position, type NodeProps } from '@xyflow/react';
 
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
@@ -15,102 +18,99 @@ import { Iconify } from 'src/components/Iconify';
 
 import NodeContext from './nodeContext';
 
-import type { NodeProps } from './type';
-
 export function StandardNode({
-  id,
-  placementPosition,
-  username,
-  fullName,
-  commission,
-  createdAt,
-}: NodeProps) {
+  data: { id, placementPosition, username, fullName, commission, createdAt },
+}: NodeProps & { data: PlacementMember }) {
   const router = useRouter();
 
   const { visibleMap, expandTree, collapseTree } = useContext(NodeContext);
 
   return (
-    <Card
-      sx={{
-        p: 2,
-        minWidth: 200,
-        borderRadius: 1.5,
-        textAlign: 'left',
-        position: 'relative',
-        display: 'inline-flex',
-        flexDirection: 'column',
-      }}
-    >
-      <Typography
-        variant="subtitle2"
-        noWrap
+    <>
+      <Handle type="target" position={Position.Top} isConnectable />
+      <Handle type="source" position={Position.Bottom} isConnectable />
+      <Card
         sx={{
-          mb: 0.5,
-          cursor: 'pointer',
-          '&:hover': { color: (theme) => theme.vars.palette.Alert.errorIconColor },
-        }}
-        onClick={() => {
-          router.push(paths.dashboard.members.edit(id));
-          router.refresh();
+          p: 2,
+          minWidth: 200,
+          borderRadius: 1.5,
+          textAlign: 'left',
+          position: 'relative',
+          display: 'inline-flex',
+          flexDirection: 'column',
         }}
       >
-        {customizeFullName(fullName)}
-      </Typography>
-
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        sx={{ mb: 0.5, background: 'translation' }}
-      >
-        <Typography variant="caption" component="div" noWrap sx={{ color: 'text.secondary' }}>
-          {username}
-        </Typography>
-
-        <Stack>
-          {placementPosition !== 'NONE' && (
-            <Label
-              variant={placementPosition === 'LEFT' ? 'soft' : 'outlined'}
-              color="info"
-              sx={{ fontSize: 10, border: placementPosition === 'LEFT' ? 'none' : 1 }}
-            >
-              {placementPosition}
-            </Label>
-          )}
-        </Stack>
-      </Stack>
-
-      <Stack direction="row" justifyContent="space-between">
-        <Typography variant="caption" color="gray" component="div" noWrap sx={{ mt: 1 }}>
-          L {commission?.begL || 0}/{commission?.newL || 0}
-        </Typography>
-        <Typography variant="caption" color="gray" component="div" noWrap sx={{ mt: 1 }}>
-          {commission?.begR || 0}/{commission?.newR || 0} R
-        </Typography>
-      </Stack>
-
-      <Stack direction="row" justifyContent="space-between" sx={{ background: 'translation' }}>
         <Typography
-          variant="caption"
-          component="div"
+          variant="subtitle2"
           noWrap
-          sx={{ color: 'text.secondary', mt: 0.5 }}
+          sx={{
+            mb: 0.5,
+            cursor: 'pointer',
+            '&:hover': { color: (theme) => theme.vars.palette.Alert.errorIconColor },
+          }}
+          onClick={() => {
+            router.push(paths.dashboard.members.edit(id));
+            router.refresh();
+          }}
         >
-          {formatDate(createdAt)}
+          {customizeFullName(fullName)}
         </Typography>
 
-        <Stack>
-          {visibleMap[id] !== 3 && (
-            <Iconify
-              icon={`mdi:${visibleMap[id] === 1 ? 'plus' : 'minus'}-circle-outline`}
-              sx={{ mt: 0.15, cursor: 'pointer' }}
-              onClick={() => {
-                if (visibleMap[id] === 1) expandTree(id);
-                else if (visibleMap[id] === 2) collapseTree(id);
-              }}
-            />
-          )}
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          sx={{ mb: 0.5, background: 'translation' }}
+        >
+          <Typography variant="caption" component="div" noWrap sx={{ color: 'text.secondary' }}>
+            {username}
+          </Typography>
+
+          <Stack>
+            {placementPosition !== 'NONE' && (
+              <Label
+                variant={placementPosition === 'LEFT' ? 'soft' : 'outlined'}
+                color="info"
+                sx={{ fontSize: 10, border: placementPosition === 'LEFT' ? 'none' : 1 }}
+              >
+                {placementPosition}
+              </Label>
+            )}
+          </Stack>
         </Stack>
-      </Stack>
-    </Card>
+
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="caption" color="gray" component="div" noWrap sx={{ mt: 1 }}>
+            L {commission?.begL || 0}/{commission?.newL || 0}
+          </Typography>
+          <Typography variant="caption" color="gray" component="div" noWrap sx={{ mt: 1 }}>
+            {commission?.begR || 0}/{commission?.newR || 0} R
+          </Typography>
+        </Stack>
+
+        <Stack direction="row" justifyContent="space-between" sx={{ background: 'translation' }}>
+          <Typography
+            variant="caption"
+            component="div"
+            noWrap
+            sx={{ color: 'text.secondary', mt: 0.5 }}
+          >
+            {formatDate(createdAt)}
+          </Typography>
+
+          <Stack>
+            {visibleMap[id] !== 3 && (
+              <Iconify
+                icon={`mdi:${visibleMap[id] === 1 ? 'plus' : 'minus'}-circle-outline`}
+                sx={{ mt: 0.15, cursor: 'pointer' }}
+                onClick={() => {
+                  if (visibleMap[id] === 1) expandTree(id);
+                  else if (visibleMap[id] === 2) collapseTree(id);
+                }}
+              />
+            )}
+          </Stack>
+        </Stack>
+      </Card>
+    </>
   );
 }
