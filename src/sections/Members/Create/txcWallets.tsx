9185e7d891c7cc9a12acefe1792b79291c42608e
Skyward <skywardwander06@gmail.com>
2025-03-11 12:01:20 -0400

    Add generate button in miner editform

--- src/sections/Members/Create/txcWallets.tsx
+++ src/sections/Members/Create/txcWallets.tsx
@@ -9,13 +9,16 @@ import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 
-import { TXC_WALLET } from 'src/consts';
+import { fetchXmlData } from 'src/utils/helper';
+
+import { TXC_WALLET, ASSET_INFO_PATH } from 'src/consts';
 
 import { Field } from 'src/components/Form';
+import { toast } from 'src/components/SnackBar';
 import { Iconify } from 'src/components/Iconify';
 
 export default function TXCWallets() {
-  const { control } = useFormContext();
+  const { watch, control } = useFormContext();
   const { fields, append, remove } = useFieldArray({ control, name: 'txcWallets' });
 
   const addWallet = () => {
@@ -23,7 +26,7 @@ export default function TXCWallets() {
       note: '',
       percent: 0,
       address: '',
-      payoutId: '',
+      payoutId: TXC_WALLET[1].id,
       isDefault: false,
     });
   };
@@ -32,11 +35,38 @@ export default function TXCWallets() {
     remove(index);
   };
 
+  const handleGenerate = async () => {
+    try {
+      const assetId = watch('assetId');
+
+      const response = await fetchXmlData(`${ASSET_INFO_PATH}${assetId}`);
+
+      if (response) {
+        append({
+          payoutId: TXC_WALLET[1].id,
+          address: response.getElementsByTagName('publicKey').item(0)?.textContent,
+          note: '',
+          percent: 0,
+          isDefault: false,
+        });
+      } else {
+        toast.error("Can't find the address from this Coin ID");
+      }
+    } catch (error) {
+      console.log('error => ', error);
+    }
+  };
+
   return (
     <Card sx={{ p: 3, mb: 2 }}>
-      <Typography sx={{ pb: 2 }} variant="subtitle1">
-        TXC Wallets
-      </Typography>
+      <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ pb: 2 }}>
+        <Typography variant="subtitle1">TXC Wallets</Typography>
+
+        <IconButton color="default" onClick={handleGenerate}>
+          <Iconify icon="streamline:ai-generate-variation-spark-solid" />
+        </IconButton>
+      </Stack>
+
       {fields.map((item, index) => (
         <Stack sx={{ mb: 1 }}>
           <Box
--- src/sections/Members/Edit/General/txcWallets.tsx
+++ src/sections/Members/Edit/General/txcWallets.tsx
@@ -10,9 +10,12 @@ import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 
-import { TXC_WALLET } from 'src/consts';
+import { fetchXmlData } from 'src/utils/helper';
+
+import { TXC_WALLET, ASSET_INFO_PATH } from 'src/consts';
 
 import { Field } from 'src/components/Form';
+import { toast } from 'src/components/SnackBar';
 import { Iconify } from 'src/components/Iconify';
 
 interface Props {
@@ -28,10 +31,8 @@ interface Wallet {
 }
 
 export default function TXCWallets({ wallets }: Props) {
-  const { control, setValue } = useFormContext();
+  const { watch, control, setValue } = useFormContext();
   const { fields, append, remove } = useFieldArray({ control, name: 'txcWallets' });
-
-  console.log('wallets => ', wallets);
 
   const forms: Wallet[] = fields?.length
     ? fields
@@ -56,7 +57,7 @@ export default function TXCWallets({ wallets }: Props) {
 
   const addWallet = () => {
     append({
-      payoutId: '',
+      payoutId: TXC_WALLET[1].id,
       address: '',
       note: '',
       percent: 0,
@@ -68,11 +69,38 @@ export default function TXCWallets({ wallets }: Props) {
     remove(index);
   };
 
+  const handleGenerate = async () => {
+    try {
+      const assetId = watch('assetId');
+
+      const response = await fetchXmlData(`${ASSET_INFO_PATH}${assetId}`);
+
+      if (response) {
+        append({
+          payoutId: TXC_WALLET[1].id,
+          address: response.getElementsByTagName('publicKey').item(0)?.textContent,
+          note: '',
+          percent: 0,
+          isDefault: false,
+        });
+      } else {
+        toast.error("Can't find the address from this Coin ID");
+      }
+    } catch (error) {
+      console.log('error => ', error);
+    }
+  };
+
   return (
     <Card sx={{ p: 3, mb: 2 }}>
-      <Typography sx={{ pb: 2 }} variant="subtitle1">
-        TXC Wallets
-      </Typography>
+      <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ pb: 2 }}>
+        <Typography variant="subtitle1">TXC Wallets</Typography>
+
+        <IconButton color="default" onClick={handleGenerate}>
+          <Iconify icon="streamline:ai-generate-variation-spark-solid" />
+        </IconButton>
+      </Stack>
+
       {forms?.map((item, index) => (
         <Stack sx={{ mb: 2 }}>
           <Stack key={item.id} sx={{ mb: 2 }}>
