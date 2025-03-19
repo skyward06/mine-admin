59873b5c1713b38ed8b2d74cb499971a4bb1f1bd
Skyward <skywardwander06@gmail.com>
2025-03-19 12:34:56 -0400

    Add invoice section

---
+++ src/sections/Invoice/List/ActoinRenderer.tsx
@@ -0,0 +1,90 @@
+import type { CustomCellRendererProps } from '@ag-grid-community/react';
+
+import { memo } from 'react';
+
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+
+import { useBoolean } from 'src/hooks/useBoolean';
+
+import { InvoiceStatusEnum } from 'src/__generated__/graphql';
+
+import { toast } from 'src/components/SnackBar';
+import { Iconify } from 'src/components/Iconify';
+import { usePopover, CustomPopover } from 'src/components/custom-popover';
+
+import Detail from './Detail';
+import { useMoveToPaid } from '../useApollo';
+
+import type { Invoice } from './type';
+
+export const ActionRender = memo(
+  ({ data }: CustomCellRendererProps<Invoice>) => {
+    const defaultValue = {
+      id: 0,
+      name: '',
+      dueDate: new Date(),
+      description: '',
+      amountInCents: 0,
+      status: InvoiceStatusEnum.Pending,
+    };
+
+    const open = useBoolean();
+    const popover = usePopover();
+
+    const { loading, moveToPaid } = useMoveToPaid();
+
+    const handlePaid = async () => {
+      try {
+        const { data: result } = await moveToPaid({ variables: { data: { ID: data?.id ?? 0 } } });
+
+        if (result) {
+          toast.success('Successfully moved!');
+          popover.onClose();
+        }
+      } catch (error) {
+        console.log('error => ', error);
+        toast.error(error);
+      }
+    };
+
+    return (
+      <>
+        <IconButton color={popover.open ? 'inherit' : 'default'} onClick={popover.onOpen}>
+          <Iconify icon="eva:more-horizontal-fill" />
+        </IconButton>
+        <CustomPopover
+          open={popover.open}
+          anchorEl={popover.anchorEl}
+          onClose={popover.onClose}
+          slotProps={{ arrow: { placement: 'right-top' } }}
+        >
+          <MenuList>
+            <MenuItem
+              onClick={() => {
+                open.onTrue();
+                popover.onClose();
+              }}
+            >
+              <Iconify icon="solar:eye-bold" />
+              View
+            </MenuItem>
+            <MenuItem
+              onClick={async () => {
+                await handlePaid();
+              }}
+              disabled={data?.status === InvoiceStatusEnum.Paid}
+            >
+              <Iconify icon={loading ? 'eos-icons:bubble-loading' : 'mdi:forward'} />
+              Move to Paid
+            </MenuItem>
+          </MenuList>
+        </CustomPopover>
+
+        <Detail open={open} row={data ?? defaultValue} />
+      </>
+    );
+  },
+  (prev, next) => prev.data?.id === next.data?.id
+);
---
+++ src/sections/Invoice/List/type.ts
@@ -0,0 +1,16 @@
+import type { Proof } from 'src/sections/Proof/List/type';
+import type { InvoiceStatusEnum } from 'src/__generated__/graphql';
+
+export type Invoice = {
+  __typename?: 'Invoice';
+  id: number;
+  name: string;
+  dueDate: any;
+  description: string;
+  proof?: Proof | null;
+  amountInCents: number;
+  status: InvoiceStatusEnum;
+  createdAt?: any | null;
+  updatedAt?: any | null;
+  deletedAt?: any | null;
+};
