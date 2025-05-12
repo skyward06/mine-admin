e74fef2bc5a84ce71d362e7db8aa4348566288e1
Skyward <skywardwander06@gmail.com>
2025-05-12 12:06:47 -0400

    Update miner type

--- src/sections/Members/List/type.ts
+++ src/sections/Members/List/type.ts
@@ -132,3 +132,23 @@ export type BasicMember = {
   secondaryAddress?: string | null;
   signUpPaymentType?: string | null;
 };
+
+// ----------------------------------------------------------------------
+
+export type MemberRole = 'approved' | 'pending' | 'graveyard';
+export type AllowState = 'PENDING' | 'GRAVEYARD' | 'APPROVED' | 'PAID' | 'BLOCKED';
+
+export type IMemberTableFilters = {
+  search: string;
+  status?: MemberRole;
+  allowState?: AllowState;
+};
+
+export type IMemberPrismaFilter = {
+  OR?: any;
+  sponsorId?: any;
+  status?: boolean;
+  allowState?: AllowState;
+  emailVerified?: boolean;
+  deletedAt?: any;
+};
