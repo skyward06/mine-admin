503d2e3390ffa96f8653d0c7bfde4965d82f4247
Skyward <skywardwander06@gmail.com>
2025-05-26 14:25:55 -0400

    Update confirm modal

--- src/sections/Members/Edit/General/Confirm.tsx
+++ src/sections/Members/Edit/General/Confirm.tsx
@@ -4,10 +4,15 @@ import type { UseBooleanReturn } from 'src/hooks/useBoolean';
 import { useNavigate } from 'react-router';
 
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import Typography from '@mui/material/Typography';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
 
 import { paths } from 'src/routes/paths';
 
-import { ConfirmDialog } from 'src/components/Dialog';
+import { toast } from 'src/components/SnackBar';
 
 interface Props {
   open: UseBooleanReturn;
@@ -18,12 +23,19 @@ export default function ConfirmCreateSale({ open, member }: Props) {
   const navigate = useNavigate();
 
   return (
-    <ConfirmDialog
+    <Dialog
       open={open.value}
-      title="Confirm"
-      onClose={open.onFalse}
-      content="Will you create a sale or not?"
-      action={
+      maxWidth="xs"
+      fullWidth
+      sx={{ [`& .css-2vtdcu-MuiPaper-root-MuiDialog-paper`]: { borderRadius: 1 } }}
+    >
+      <DialogTitle>Confirm</DialogTitle>
+
+      <DialogContent sx={{ py: 7 }}>
+        <Typography>Will you create a sale or not?</Typography>
+      </DialogContent>
+
+      <DialogActions>
         <Button
           variant="contained"
           color="primary"
@@ -35,7 +47,16 @@ export default function ConfirmCreateSale({ open, member }: Props) {
         >
           OK
         </Button>
-      }
-    />
+        <Button
+          variant="outlined"
+          onClick={() => {
+            navigate(paths.dashboard.members.root);
+            toast.success('Updated successfully!');
+          }}
+        >
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
