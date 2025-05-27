ecfcd771bf4e6219961498381bfcaea3589a05ee
Skyward <skywardwander06@gmail.com>
2025-05-27 13:12:36 -0400

    Update weeklycommisison query

--- src/sections/Commission/query.ts
+++ src/sections/Commission/query.ts
@@ -23,6 +23,7 @@ export const FETCH_COMMISSION_QUERY = gql(/* GraphQL */ `
         username
         fullName
         memberId
+        hasUSDC
         createdAt
         qualified
         shortNote
