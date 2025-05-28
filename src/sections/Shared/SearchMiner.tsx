883155090c1bf105b48852f0591adc6ca547d05c
Skyward <skywardwander06@gmail.com>
2025-05-28 12:21:11 -0400

    Update search miner component

--- src/sections/Shared/SearchMiner.tsx
+++ src/sections/Shared/SearchMiner.tsx
@@ -49,7 +49,9 @@ export default function SearchMiner({ currentMembers, setMemberIds }: Props) {
   }, [username]);
 
   useEffect(() => {
-    setCurrent(currentMembers.map((item) => `${item?.id}()${item?.username}()${item?.fullName}`));
+    if (currentMembers.length) {
+      setCurrent(currentMembers.map((item) => `${item?.id}()${item?.username}()${item?.fullName}`));
+    }
   }, [currentMembers]);
 
   return (
